79a5948ef4eb00f08b484ec7f6d88764c4cd4b3b
isthatcentered <e.peninb@gmail.com>
2020-02-01 10:10:08 +0100

    refactor: Use ts for page template

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -1,76 +1,74 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm, scale } from "../utils/typography"
 
-const PageTemplate = (props) =>(
-  <div>
+
+
+function BlogPostTemplate( props:any )
+{
+  const post = props.data.mdx
+  const siteTitle = props.data.site.siteMetadata.title
+  const { previous, next } = props.pageContext
   
-  </div>
-)
-
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.mdx
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          title={post.frontmatter.title}
-          description={post.frontmatter.description  }
-        />
-        <h1>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-          {post.frontmatter.date}
-        </p>
-        <MDXRenderer>{post.body}</MDXRenderer>
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
-        <Bio />
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={`blog${previous.fields.slug}`} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
-            )}
-          </li>
-          <li>
-            {next && (
-              <Link to={`blog${next.fields.slug}`} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
-      </Layout>
-    )
-  }
+  return (
+    <Layout>
+      <SEO
+        meta={[]}
+        title={post.frontmatter.title}
+        description={post.frontmatter.description}
+      />
+      <h1>{post.frontmatter.title}</h1>
+      <p
+        // style={{
+//          ...scale( -1 / 5 ),
+//           display:      `block`,
+//           marginBottom: rhythm( 1 ),
+//           marginTop:    rhythm( -1 ),
+        //}}
+      >
+        {post.frontmatter.date}
+      </p>
+      <MDXRenderer>{post.body}</MDXRenderer>
+      <hr
+        style={{
+          // marginBottom: rhythm( 1 ),
+        }}
+      />
+      <Bio/>
+      
+      <ul
+        style={{
+          display:        `flex`,
+          flexWrap:       `wrap`,
+          justifyContent: `space-between`,
+          listStyle:      `none`,
+          padding:        0,
+        }}
+      >
+        <li>
+          {previous && (
+            <Link to={`blog${previous.fields.slug}`}
+                  rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+        </li>
+        <li>
+          {next && (
+            <Link to={`blog${next.fields.slug}`}
+                  rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </li>
+      </ul>
+    </Layout>
+  )
 }
+
 
 export default BlogPostTemplate
 
@@ -84,7 +82,7 @@ export const pageQuery = graphql`
     }
     mdx(fields: { slug: { eq: $slug } }) {
       id
-#      excerpt(pruneLength: 160)
+      #      excerpt(pruneLength: 160)
       body
       frontmatter {
         title
